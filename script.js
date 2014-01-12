44f8bc8f0309334788f97ba0a6897b354b78e48e
Jimmy Thomas <jimminikin@gmail.com>
2014-01-12 20:31:07 +0100

    routing

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 angular
-.module('PomodoroApp', [])
+.module('PomodoroApp', ["ngRoute"])
 .controller("PomodoroCtrl", function PomodoroCtrl($scope, PomodoroService) {
 	$scope.tasks = PomodoroService.data;
 	
@@ -16,11 +16,43 @@ angular
 		delete $scope.estimatedCount;
 	}
 })
+.controller("PomodoroDetailCtrl", function PomodoroCtrl($scope, PomodoroService, $routeParams) {
+	$scope.task = PomodoroService.get($routeParams.id);
+})
 .service("PomodoroService", function(){
-	this.data = [];
+	this.data = [
+		{
+			"name": "Faire des crêpes",
+			"estimatedCount": 2,
+			"doneCount": 0
+		},
+		{
+			"name": "Manger des crêpes",
+			"estimatedCount": 1,
+			"doneCount": 0
+		}
+	];
 
 	this.add = function(item){
 		this.data.push(item);
 	}
+
+	this.get = function(index){
+		return this.data[index];
+	}
+})
+.config(function($routeProvider){
+	$routeProvider.
+		when('/list', {
+			templateUrl: 'partials/list.html',
+			controller: "PomodoroCtrl"
+		}).
+		when('/detail/:id', {
+			templateUrl: 'partials/detail.html',
+			controller: "PomodoroDetailCtrl"
+		}).
+		otherwise({
+			redirectTo: "/list"
+		})
 });
 
