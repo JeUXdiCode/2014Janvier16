a0740c70dd8decbf79d375cffa3eba1f90b88b77
Jimmy Thomas <jimminikin@gmail.com>
2014-01-15 23:58:13 +0100

    Express

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 "use strict";
-var http = require('http');
+var express = require('express')
+  , app = express();
 
-http.createServer(function (request, response) {
-	
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World');
-	
-	console.log(request.url);
-}).listen(8080);
+app.use(express.static(__dirname+'/app'));
 
-console.log('Serveur démarré sur http://127.0.0.1:8080/');
+app.listen(8080, "127.0.0.1");
