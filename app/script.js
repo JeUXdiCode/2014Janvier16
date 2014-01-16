625081c413d9f64af10041db9b12f9b7a23d8328
Matthieu ROBERT <matthieu.robert@gmail.com>
2014-01-16 17:40:30 +0100

    Add server

--- app/script.js
+++ app/script.js
@@ -1,99 +1,96 @@
 "use strict";
 
 angular
-.module('PomodoroApp', ["ngRoute"])
+.module('PomodoroApp', ["ngRoute","ngResource"])
 .controller("PomodoroCtrl", function PomodoroCtrl($scope, PomodoroService) {
-	$scope.tasks = PomodoroService.data;
-	
-	$scope.add = function() {
-		PomodoroService.add({
-			name: $scope.name,
-			estimatedCount: $scope.estimatedCount,
-			doneCount: 0
-		});
-		
-		delete $scope.name;
-		delete $scope.estimatedCount;
-	}
+    $scope.tasks = PomodoroService.list();
+    
+    $scope.add = function() {
+        PomodoroService.add({
+            name: $scope.name,
+            estimatedCount: $scope.estimatedCount,
+            doneCount: 0
+        });
+        
+        delete $scope.name;
+        delete $scope.estimatedCount;
+    }
 })
 .controller("PomodoroDetailCtrl", function PomodoroCtrl($scope, PomodoroService, $routeParams) {
-	$scope.task = PomodoroService.get($routeParams.id);
+    $scope.task = PomodoroService.get($routeParams.id);
 })
-.service("PomodoroService", function(){
-	this.data = [
-		{
-			"name": "Faire des crêpes",
-			"estimatedCount": 2,
-			"doneCount": 0
-		},
-		{
-			"name": "Manger des crêpes",
-			"estimatedCount": 1,
-			"doneCount": 0
-		}
-	];
-
-	this.add = function(item){
-		this.data.push(item);
-	}
-
-	this.get = function(index){
-		return this.data[index];
-	}
+.service("PomodoroService", function($resource){
+    
+    this.list = function(){
+        var Pomodoro = $resource('/list');
+        return Pomodoro.query();
+    }
+    
+    this.add = function(item){
+        var Pomodoro = $resource('/add');
+        var pom = new Pomodoro(item);
+        pom.$save();        
+    }
+    
+    this.get = function(index){
+        var Pomodoro = $resource('/get/:index', {index:'@id'});
+        var pom = Pomodoro.get({index:index});
+        return pom;    
+    }
 })
 .config(function($routeProvider){
-	$routeProvider.
-		when('/list', {
-			templateUrl: 'partials/list.html',
-			controller: "PomodoroCtrl"
-		}).
-		when('/detail/:id', {
-			templateUrl: 'partials/detail.html',
-			controller: "PomodoroDetailCtrl"
-		}).
-		otherwise({
-			redirectTo: "/list"
-		})
+    $routeProvider.
+    when('/list', {
+        templateUrl: 'partials/list.html',
+        controller: "PomodoroCtrl"
+    }).
+    when('/detail/:id', {
+        templateUrl: 'partials/detail.html',
+        controller: "PomodoroDetailCtrl"
+    }).
+    otherwise({
+        redirectTo: "/list"
+    })
 })
 .directive("countdown", function() {
-	return {
-		restrict: 'E',
-		template: '<button class="btn btn-default">' +
-			'<span ng-if="!running" ng-click="startTimer()" class="glyphicon glyphicon-play"></span>' +
-			'<span ng-if="running" ng-click="stopTimer()" class="glyphicon glyphicon-stop"> {{ getTime() }}</span>' +
-		'</button>',
-		scope: {
-			increment: '=increment'
-		},
-		controller: function($scope, $interval){
-			$scope.init = function(){
-				$scope.running = false;
-				$scope.time = 25 * 60;
-			};
-
-			$scope.getTime = function(){
-				return Math.floor($scope.time/60) + ':' + $scope.time%60;
-			}
-
-			$scope.startTimer = function(){
-
-				$scope.running = true;
-
-				$scope.interval = $interval(function(){
-					if($scope.time > 0){
-						$scope.time --;
-					}
-				}, 1000);
-			}
-
-			$scope.stopTimer = function(){
-				$interval.cancel($scope.interval);
-				$scope.init();
-				$scope.increment++;
-			}
-
-			$scope.init();
-		}
-	}
+    return {
+        restrict: 'E',
+        template: '<button class="btn btn-default">' +
+        '<span ng-if="!running" ng-click="startTimer()" class="glyphicon glyphicon-play"></span>' +
+        '<span ng-if="running" ng-click="stopTimer()" class="glyphicon glyphicon-stop"> {{ getTime() }}</span>' +
+        '</button>',
+        scope: {
+            increment: '=increment'
+        },
+        controller: function($scope, $interval){
+            $scope.init = function(){
+                $scope.running = false;
+                $scope.time = 25 * 60;
+            };
+            
+            $scope.getTime = function(){
+                return Math.floor($scope.time/60) + ':' + $scope.time%60;
+            }
+            
+            $scope.startTimer = function(){
+                
+                $scope.running = true;
+                
+                $scope.interval = $interval(function(){
+                    if($scope.time > 0){
+                        $scope.time --;
+                    }
+                }, 1000);
+            }
+            
+            $scope.stopTimer = function(){
+                $interval.cancel($scope.interval);
+                $scope.init();
+                $scope.increment++;
+            }
+            
+            $scope.init();
+        }
+    }
 });
 
--- server.js
+++ server.js
@@ -2,6 +2,38 @@
 var express = require('express')
   , app = express();
 
+app.use(express.bodyParser());
 app.use(express.static(__dirname+'/app'));
 
 app.listen(8080, "127.0.0.1");
+
+var data = [
+		{
+			"name": "Faire des crêpes",
+			"estimatedCount": 2,
+			"doneCount": 0
+		},
+		{
+			"name": "Manger des crêpes",
+			"estimatedCount": 1,
+			"doneCount": 0
+		}
+	];
+
+
+app.get('/list', function(req, res, next) {     
+   return res.send(data);	
+});
+
+app.get('/get/:index', function(req, res, next) {
+    var id = req.params.index; 
+   return res.send(data[index]);	
+});
+
+app.post('/add', function(req, res){
+    console.log(req);   
+    var item = req.body;
+    data.push(item);
+    console.log(item);
+    res.send(item);
+});
